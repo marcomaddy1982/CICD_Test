9b8a050c58cc9bacc167058295597bb8229a5026
marcomaddy1982 <marco.maddalena1982@gmail.com>
2022-07-14 12:18:46 +0200

    Initial Commit

---
+++ CICD_Project/CICD_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_ProjectApp.swift
+//  CICD_Project
+//
+//  Created by Marco Maddalena on 14.07.22.
+//
+
+import SwiftUI
+
+@main
+struct CICD_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICD_Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CICD_Project
+//
+//  Created by Marco Maddalena on 14.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
